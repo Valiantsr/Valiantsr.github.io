d5ab635ab8b7bda6127420a322f5d3adf4fcb9a1
Valiant <valiant.shabri@gmail.com>
2019-12-17 12:35:52 +0700

    Update Modal

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+function check_empty() {
+    var email=document.getElementById('email').value;
+    if (email == "") {
+    alert("No email address specified.");
+    } else {
+    document.getElementById('forgetpass').submit("login.html");
+    alert("Your password has been reset, please check Your email");
+    window.location.href = "login.html";
+    }
+    }
+    //Function To Display Popup
+    function show_popup() {
+    document.getElementById('isipopup').style.display = "block";
+    }
+    //Function to Hide Popup
+    function hide_popup(){
+    document.getElementById('isipopup').style.display = "none";
+    }
